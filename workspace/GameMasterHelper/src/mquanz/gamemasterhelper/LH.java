98ebe6d3c6065052f841655629a92b0f6f3267c3
MarlQ <thestrandedalliance@gmail.com>
2017-12-17 15:10:05 +0100

    commenting

--- workspace/GameMasterHelper/src/mquanz/gamemasterhelper/LH.java
+++ workspace/GameMasterHelper/src/mquanz/gamemasterhelper/LH.java
@@ -4,6 +4,23 @@ import java.awt.*;
 
 abstract class LH {
 
+
+	/**
+	 * Places a component using an existing {@link GridBagLayout} in a container.
+	 * @param x gridx
+	 * @param y gridy
+	 * @param w gridwidth
+	 * @param h gridheight
+	 * @param wx weightx
+	 * @param wy weigthty
+	 * @param fill specifies how the component stretches out if it has space. Can be n (NONE), h (HORIZONTAL), v (VERTICAL) or b (BOTH).
+	 * @param anchor specifies how the component is laid out. Can be n (NORTH), w (WEST), e (EAST), s (South), nw, ne, sw or se.
+	 * @param insets inserts additional empty space between components. Does not add space if null.
+	 * @param p the Container in which the component should be laid out.
+	 * @param c the GridBagConstraints of the existing GridBagLayout.
+	 * @param component the component that is to be laid out.
+	 */
+
 	static void place(int x, int y, int w, int h, double wx, double wy, String fill, String anchor, Insets insets, Container p, GridBagConstraints c,Component component){
 		c.gridx = x;
 		c.gridy = y;
