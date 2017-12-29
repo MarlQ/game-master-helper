2755977653fd666d4eb16426d47f8a9060fec01d
MarlQ <marcel.quanz@gmail.com>
2017-12-29 23:24:00 +0100

    Added class.

---
+++ workspace/GameMasterHelper/src/mquanz/gamemasterhelper/ControllerMapObject.java
@@ -0,0 +1,26 @@
+package mquanz.gamemasterhelper;
+
+import java.util.ArrayList;
+
+public class ControllerMapObject {
+
+    private GUIMain mainFrame;
+
+    ControllerMapObject(GUIMain mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+    
+    void deleteObjectOnMap(MapObjectIcon objectIcon, MapInformation map){
+
+    }
+    void deleteObjectsOnMap(ArrayList<MapObjectIcon> objectIcons,  MapInformation map){
+
+    }
+    void deleteObjectCompletely(MapObjectIcon objectIcon){
+
+    }
+    void deleteObjectsCompletely(ArrayList<MapObjectIcon> objectIcons){
+
+    }
+    
+}
