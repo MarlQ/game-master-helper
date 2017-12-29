14e36c90ae679d7b92a8068f3097eef42a9fc3ff
MarlQ <marcel.quanz@gmail.com>
2017-12-29 14:31:39 +0100

    Implemented changeMapSize(), added changeMapName() - yet to implement.

--- workspace/GameMasterHelper/src/mquanz/gamemasterhelper/ControllerMap.java
+++ workspace/GameMasterHelper/src/mquanz/gamemasterhelper/ControllerMap.java
@@ -2,6 +2,7 @@ package mquanz.gamemasterhelper;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ControllerMap{
 
@@ -35,6 +36,40 @@ public class ControllerMap{
 
         DefaultListModel model = (DefaultListModel) mainFrame.topPane.dialogMapList.list.getModel();
         model.removeElement(map);
+    }
+    void changeMapName(MapInformation map, String name){
+        map.name = name;
+        //TODO:
+    }
+
+    void changeMapSize(MapInformation map, int width, int height, boolean removeObjects){
+        map.mapSize.width = width;
+        map.mapSize.height = height;
+
+            //Remove Objects out of bounds
+        ArrayList<MapObjectIcon> toRemove = new ArrayList<>();
+            for(MapObjectIcon mapObjectIcon : map.itemIcons) {
+                int iconWidth = mapObjectIcon.mapObject.icon.getIconWidth();
+                int iconHeight = mapObjectIcon.mapObject.icon.getIconHeight();
+                System.out.println("Width " + iconWidth + " Height " + iconHeight);
+                if (mapObjectIcon.posX+iconWidth > width || mapObjectIcon.posY+iconHeight > height) {
+                    if(mapObjectIcon.isSelected){
+                        mainFrame.drawingSurface.dragScrollPane.selectedIcon = null;
+                    }
+                    if(removeObjects) {
+                        //Remove non-instanced objects
+                        mainFrame.generalInformation.objects.remove(mapObjectIcon.mapObject);
+                    }
+                    mainFrame.drawingSurface.componentMover.deregisterComponent(mapObjectIcon);
+                    mainFrame.drawingSurface.remove(mapObjectIcon);
+                    toRemove.add(mapObjectIcon);
+                }
+            }
+            map.itemIcons.removeAll(toRemove);
+        mainFrame.drawingSurface.setMaximumSize(map.mapSize);
+        mainFrame.drawingSurface.setPreferredSize(map.mapSize);
+        mainFrame.drawingSurface.revalidate();
+        mainFrame.drawingSurface.repaint();
     }
 
     void changeMap(MapInformation map){
