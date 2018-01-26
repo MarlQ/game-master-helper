d17c99e7912c4833ad744cf758fc5d5b394d48fc
MarlQ <thestrandedalliance@gmail.com>
2018-01-26 10:52:42 +0100

    Was missing from last commit.

--- workspace/GameMasterHelper/src/mquanz/gamemasterhelper/MapLink.java
+++ workspace/GameMasterHelper/src/mquanz/gamemasterhelper/MapLink.java
@@ -6,6 +6,11 @@ public class MapLink extends MapObject {
 
 	int linkPosX, linkPosY;
 	MapInformation map;
+	MapLink twoWayLink;
+	Boolean isTwoWayLink;
+
+	MapInformation ownMap;
+	int ownPosX, ownPosY;
 
 	public MapLink(String name, Color textColor, int linkPosX, int linkPosY, MapInformation map,
 			MapLinkType mapLinkType, ImageIcon icon) {
@@ -25,6 +30,8 @@ public class MapLink extends MapObject {
 		this.map = map;
 		this.type = mapLinkType;
 		this.className = "Map Link";
+		this.twoWayLink = null;
+		this.isTwoWayLink = false;
 	}
 
 }
